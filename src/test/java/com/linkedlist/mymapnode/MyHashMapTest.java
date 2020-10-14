18ecdb153c96bda5d05d1eed9b786150d324f596
jangle <shubhamjangale0000@gmail.com>
2020-10-14 22:59:18 +0530

    Added : Added functionality to find the frequency of a given key word

--- src/test/java/com/linkedlist/mymapnode/MyHashMapTest.java
+++ src/test/java/com/linkedlist/mymapnode/MyHashMapTest.java
@@ -5,19 +5,17 @@ import org.junit.jupiter.api.Test;
 
 class MyHashMapTest {
 
-	
-	
 	@Test
 	public void GivenASentence_WhenAddedtoList_ShouldGiveFrequencyOfWords() {
 		String sentence = "to be or not to be";
 		MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
 		String words[] = sentence.toLowerCase().split(" ");
-		
-		for(String word : words) {
+
+		for (String word : words) {
 			Integer value = myHashMap.get(word);
-			if(value == null) {
+			if (value == null) {
 				value = 1;
-			}
+			} 
 			else {
 				value++;
 			}
